0e55838b22d6349e45a6acef0ee56db97f7c0f86
Richard Jensen <facetnated@gmail.com>
2014-09-19 06:08:05 -0500

    Richard -- A failing and a table driven test

--- src/test/java/com/rhjensen/examples/spock/BasicSpockTest.groovy
+++ src/test/java/com/rhjensen/examples/spock/BasicSpockTest.groovy
@@ -1,5 +1,6 @@
 package com.rhjensen.examples.spock
 
+import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
@@ -11,5 +12,34 @@ class BasicSpockTest extends Specification {
     def "It should just work"() {
         expect:
             true == true
+    }
+
+    /**
+     *
+     * Condition not satisfied:
+
+     * a == b
+     * | |  |
+     * 6 |  7
+     * false
+     *
+     */
+    @Ignore
+    def "A failure gives detailed information()"() {
+        def a = 42 / 7
+        def b = 35 / 5
+        expect:
+            a == b
+    }
+
+    def "It is easy to create 'parameterized' tests"() {
+        expect:
+            a == b
+        where:
+            a | b
+            1 | 1
+            true | true
+            false | !true
+            3 * 5 | 45 / 3
     }
 }
